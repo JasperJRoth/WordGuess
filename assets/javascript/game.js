195ce3b347c7b28b763724bc3c8caf3db6ddc8aa
JasperJRoth <Jasper.J.Roth@gmail.com>
2019-06-11 20:46:36 -0400

    Added some polish, im calling it done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,15 @@ function getRandomCountry(){
 }
 
 var country = getRandomCountry().name;
+console.log(country);
+
 var guessList = [];
+var guessesRemaining = 14;
 
 function updateGuessWord(){
     var word = "";
     var guessWord = new Array(country.length);
+    var isGameWon = true;
 
     for(var i = 0; i < guessWord.length; i++){
         guessWord[i] = {
@@ -27,24 +31,44 @@ function updateGuessWord(){
         if(guessWord[i].letter === " "){
             word += "- ";
         }else if(!guessWord[i].visisble){
+            isGameWon = false;
             word += "_ ";
         }else{
             word += `${guessWord[i].letter} `;
         }
     }
 
+    $("#guessesRemaining").text(`You have ${guessesRemaining} guesses remaining!`);
+
     $("#guessWord").text(word);
+
+    if(isGameWon){
+        alert("YOU WIN!");
+        
+        country = getRandomCountry().name;
+        console.log(country);
+        guessesRemaining = 14;
+        guessList = [];
+        updateGuessWord();
+    }
 }
 
 $(document).keypress(function(event){
     if(!guessList.includes(event.key.toLowerCase())){
         guessList.push(event.key.toLowerCase());
+        guessesRemaining -= 1;
+        if(guessesRemaining <= 0){
+            alert("NO MORE GUESSES, GAME OVER!");
+            
+            country = getRandomCountry().name;
+            console.log(country);
+            guessesRemaining = 14;
+            guessList = [];
+        }
         updateGuessWord();
     }
 });
 
 $(document).ready(function(){
     updateGuessWord();
-
-    console.log(country);
 });
